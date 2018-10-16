f75e869106318a25ed7683aa28979e62259891ad
Test0004 <TheBlockedCrafter@gmail.com>
2018-10-15 22:35:02 -0400

    more commands and garbage

--- PokecordCatcher/Services/CommandService.cs
+++ PokecordCatcher/Services/CommandService.cs
@@ -119,6 +119,10 @@ namespace PokecordCatcherBot.Services
         public async Task Confirm(SocketMessage msg, string[] args) => 
             await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
 			
+        [Command(nameof(Shiny), "Checks for shinies.")]
+        public async Task Shiny(SocketMessage msg, string[] args) => 
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}pokemon --shiny");
+			
         [Command(nameof(StartTrade), "Starts a trade with the bot owner.")]
         public async Task StartTrade(SocketMessage msg, string[] args) => 
             await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}trade <@{Configuration.OwnerID}>");
@@ -167,25 +171,20 @@ namespace PokecordCatcherBot.Services
 
             if (trade == null)
             {
-                await msg.Channel.SendMessageAsync("Pokecord didn't create trade, aborting.");
+                await msg.Channel.SendMessageAsync("p!cancel");
                 return;
             }
 
             await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add {String.Join(' ', pokemans.Select(x => x.Id))}");
-            await Task.Delay(1500);
+            await Task.Delay(1000);
             await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
             await Task.Delay(1500);
         }
 
-        [Command(nameof(TradeById), "Trades a pokemon with a certain ID.")]
+        [Command(nameof(TradeById), "Trades pokemon with specified ids.")]
         public async Task TradeById(SocketMessage msg, string[] args)
         {
-            if (args.Length != 1)
-            {
-                await msg.Channel.SendMessageAsync("Invalid arguments. Please supply an ID.");
-                return;
-            }
-
+            
             var trade = await ResponseGrabber.SendMessageAndGrabResponse(
                 (ITextChannel)msg.Channel,
                 $"{Configuration.PokecordPrefix}trade <@{Configuration.OwnerID}>",
@@ -197,14 +196,134 @@ namespace PokecordCatcherBot.Services
 
             if (trade == null)
             {
-                await msg.Channel.SendMessageAsync("Pokecord didn't create trade, aborting.");
+                await msg.Channel.SendMessageAsync("p!cancel");
                 return;
             }
 
-            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add {args[0]}");
-            await Task.Delay(3000);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add {String.Join(' ', args)}");
+            await Task.Delay(1000);
             await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
-            await Task.Delay(3000);
+            await Task.Delay(1500);
         }
+		
+		[Command(nameof(Trade100), "Trades pokemon with specified ids.")]
+        public async Task Trade100(SocketMessage msg, string[] args)
+        {
+            
+            var trade = await ResponseGrabber.SendMessageAndGrabResponse(
+                (ITextChannel)msg.Channel,
+                $"{Configuration.PokecordPrefix}trade <@{Configuration.OwnerID}>",
+                x => MessagePredicates.TradeMessage(x, msg),
+                5
+            );
+
+            await Task.Delay(3000);
+
+            if (trade == null)
+            {
+                await msg.Channel.SendMessageAsync("p!cancel");
+                return;
+            }
+
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25");
+            await Task.Delay(1523);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50");
+            await Task.Delay(1927);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75");
+            await Task.Delay(1750);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100");
+            await Task.Delay(1853);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
+            await Task.Delay(1582);
+        }
+		
+		[Command(nameof(Trade200), "Trades pokemon with specified ids.")]
+        public async Task Trade200(SocketMessage msg, string[] args)
+        {
+            
+            var trade = await ResponseGrabber.SendMessageAndGrabResponse(
+                (ITextChannel)msg.Channel,
+                $"{Configuration.PokecordPrefix}trade <@{Configuration.OwnerID}>",
+                x => MessagePredicates.TradeMessage(x, msg),
+                5
+            );
+
+            await Task.Delay(3000);
+
+            if (trade == null)
+            {
+                await msg.Channel.SendMessageAsync("p!cancel");
+                return;
+            }
+
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25");
+            await Task.Delay(1523);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50");
+            await Task.Delay(1927);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75");
+            await Task.Delay(1750);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100");
+            await Task.Delay(1853);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125");
+            await Task.Delay(2836);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150");
+            await Task.Delay(1457);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175");
+            await Task.Delay(1953);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200");
+            await Task.Delay(2563);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
+            await Task.Delay(1636);
+        }
+		
+		[Command(nameof(Trade325), "Trades pokemon with specified ids.")]
+        public async Task Trade325(SocketMessage msg, string[] args)
+        {
+            
+            var trade = await ResponseGrabber.SendMessageAndGrabResponse(
+                (ITextChannel)msg.Channel,
+                $"{Configuration.PokecordPrefix}trade <@{Configuration.OwnerID}>",
+                x => MessagePredicates.TradeMessage(x, msg),
+                5
+            );
+
+            await Task.Delay(3000);
+
+            if (trade == null)
+            {
+                await msg.Channel.SendMessageAsync("p!cancel");
+                return;
+            }
+
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25");
+            await Task.Delay(1523);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50");
+            await Task.Delay(1927);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75");
+            await Task.Delay(1750);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100");
+            await Task.Delay(1853);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125");
+            await Task.Delay(2534);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150");
+            await Task.Delay(1463);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175");
+            await Task.Delay(2684);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200");
+            await Task.Delay(1636);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 225");
+            await Task.Delay(2337);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 226 227 228 229 230 231 232 233 234 235 236 237 238 239 240 241 242 243 244 245 246 247 248 249 250");
+            await Task.Delay(1478);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 251 252 253 254 255 256 257 258 259 260 261 262 263 264 265 266 267 268 269 270 271 272 273 274 275");
+            await Task.Delay(1735);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 276 277 278 279 280 281 282 283 284 285 286 287 288 289 290 291 292 293 294 295 296 297 298 299 300");
+            await Task.Delay(1468);
+			await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}p add 301 302 303 304 305 306 307 308 309 310 311 312 313 314 315 316 317 318 319 320 321 322 323 324 325");
+            await Task.Delay(1735);
+            await msg.Channel.SendMessageAsync($"{Configuration.PokecordPrefix}confirm");
+            await Task.Delay(1736);
+        }
+
     }
 }
